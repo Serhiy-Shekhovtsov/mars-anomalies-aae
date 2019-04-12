e6bcadf7514f7f862b180399fc5319b9a364d75d
Serhiy Shekhovtsov <serhiy.shekhovtsov@gmail.com>
2019-04-12 14:48:58 +0300

    added image extractor to `extract_patches.py`

--- extract_patches.py
+++ extract_patches.py
@@ -1,5 +1,30 @@
 import numpy as np
+from PIL import Image, ContainerIO
+import pvl
+import matplotlib.pyplot as plt
 
+
+def extract_img(image_file: str):
+    """Extracts and returns embedded image from PDS3 IMG files as PIL Image"""
+    
+    # Parsing label
+    label = pvl.load(image_file)  # load label from .IMG file
+    image_data = label['IMAGE']   # getting image object info
+    h_img, w_img = image_data[0][-1], image_data[1][-1]  # real image sizes
+    pref, suff = image_data[6][-1], image_data[7][-1]    # buffer pixels margins
+    w_total = w_img + pref + suff                        # width with margins
+
+    offset = label['^IMAGE'].value  # pointer where image is located
+    size = label['^GAP_TABLE'].value - label['^IMAGE'].value  # image size (in bytes)
+    
+    # Now getting back to file
+    with open(image_file, "rb") as f:
+        container = ContainerIO.ContainerIO(f, offset-1, size)
+        data = container.read()  # reading image bytes
+        img = Image.frombytes('L', (w_total, h_img), data, "raw")  # decoding
+        img = img.crop((pref, 0, w_img + pref, h_img))  # cropping margins
+    
+    return img
 
 def get_patch_indexes(img_size, patch_size):
     patches_n = img_size / patch_size
